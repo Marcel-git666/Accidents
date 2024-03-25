ea7cce0c477d3fbd07aa2616303450ab2d70ecae
Marcel Mravec <marcel.mravec@gmail.com>
2024-03-25 21:12:49 +0100

    fix data model is now JSONencoded and decode before using core data.

--- Accidents/Repositories/CoreDataRepository.swift
+++ Accidents/Repositories/CoreDataRepository.swift
@@ -13,79 +13,19 @@ class CoreDataRepository: AccidentReportRepository {
     init() {
         self.context = CoreDataManager.shared.viewContext()
     }
+    
     func createAccidentReportData(from report: AccidentReport) -> AccidentReportData {
         let newReportData = NSEntityDescription.insertNewObject(forEntityName: K.reportEntity, into: context) as! AccidentReportData
         
-        // Populate AccidentReportData properties using AccidentReport data
-        newReportData.accidentLocation = createAccidentLocationData(from: report.accidentLocation)
-        newReportData.driver = createDriverData(from: report.driver)
-        newReportData.otherDriver = report.otherDriver.map { createDriverData(from: $0) }
-        newReportData.accidentDescription = createAccidentDescriptionData(from: report.accidentDescription)
-        newReportData.idReport = report.id
-        return newReportData
-    }
-    
-    // Helper methods for creating related Core Data objects
-    private func createAccidentLocationData(from accidentLocation: AccidentLocation) -> AccidentLocationData {
-        let newAccidentLocationData = NSEntityDescription.insertNewObject(forEntityName: K.locationEntity, into: context) as! AccidentLocationData
-        
-        newAccidentLocationData.date = accidentLocation.date
-        newAccidentLocationData.city = accidentLocation.city
-        newAccidentLocationData.street = accidentLocation.street
-        newAccidentLocationData.houseNumber = accidentLocation.houseNumber
-        newAccidentLocationData.kilometerReading = accidentLocation.kilometerReading ?? 0
-        newAccidentLocationData.injuries = accidentLocation.injuries
-        
-        // Convert [Witness] array to NSSet of WitnessData
-        if let witnesses = accidentLocation.witnesses {
-            let witnessDataSet = NSMutableSet()
-            for witness in witnesses {
-                let witnessData = createWitnessData(from: witness)
-                witnessDataSet.add(witnessData)
-            }
-            newAccidentLocationData.witnesses = witnessDataSet
+        do {
+            let reportData = try JSONEncoder().encode(report)
+            newReportData.reportData = reportData
+            newReportData.idReport = report.id
+        } catch {
+            print("Error encoding accident report data:", error)
         }
-        newAccidentLocationData.otherDamage = accidentLocation.otherDamage ?? false
-        newAccidentLocationData.policeInvolved = accidentLocation.policeInvolved ?? false
         
-        return newAccidentLocationData
-    }
-    
-    private func createWitnessData(from witness: Witness) -> WitnessData {
-        let newWitnessData = NSEntityDescription.insertNewObject(forEntityName: K.witnessEntity, into: context) as! WitnessData
-        
-        newWitnessData.name = witness.name
-        newWitnessData.address = witness.address
-        newWitnessData.phoneNumber = witness.phoneNumber
-        
-        return newWitnessData
-    }
-    
-    private func createDriverData(from driver: Driver) -> DriverData {
-        let newDriverData = NSEntityDescription.insertNewObject(forEntityName: K.driverEntity, into: context) as! DriverData
-        
-        newDriverData.name = driver.name
-        newDriverData.address = driver.address
-        newDriverData.phoneNumber = driver.phoneNumber
-        newDriverData.driverLicenseNumber = driver.driverLicenseNumber
-        newDriverData.vehicleRegistrationPlate = driver.vehicleRegistrationPlate
-        newDriverData.insuranceCompany = driver.insuranceCompany
-        newDriverData.insurancePolicyNumber = driver.insurancePolicyNumber
-        newDriverData.isAtFault = driver.isAtFault ?? false
-        
-        return newDriverData
-    }
-    
-    private func createAccidentDescriptionData(from accidentDescription: AccidentDescription) -> AccidentDescriptionData {
-        let newAccidentDescriptionData = NSEntityDescription.insertNewObject(forEntityName: K.descriptionEntity, into: context) as! AccidentDescriptionData
-        
-        newAccidentDescriptionData.notes1 = accidentDescription.notes1
-        newAccidentDescriptionData.notes2 = accidentDescription.notes2
-        
-        newAccidentDescriptionData.vehicleDamage1 = accidentDescription.vehicleDamage1
-        newAccidentDescriptionData.vehicleDamage2 = accidentDescription.vehicleDamage2
-
-        return newAccidentDescriptionData
+        return newReportData
     }
     
     func saveReport(_ report: AccidentReport) async throws {
@@ -98,76 +38,16 @@ class CoreDataRepository: AccidentReportRepository {
         }
     }
     
-    
     func fetchAll() async throws -> [AccidentReport] {
         let fetchRequest: NSFetchRequest<AccidentReportData> = NSFetchRequest(entityName: K.reportEntity)
         
         do {
             let fetchedReports = try context.fetch(fetchRequest)
-            let reports = fetchedReports.map { fetchedReportData in
-                // Convert Core Data object to AccidentReport
-                return convertFromCoreData(fetchedReportData: fetchedReportData)
-            }
+            let reports = fetchedReports.compactMap { convertFromCoreData(fetchedReportData: $0) }
             return reports
         } catch {
             throw CoreDataError.coreDataFetchError
         }
-    }
-    
-    // Helper method to convert from Core Data object to AccidentReport
-    private func convertFromCoreData(fetchedReportData: AccidentReportData) -> AccidentReport {
-        var driver: Driver?
-        var otherDriver: Driver?
-        var accidentDescription: AccidentDescription
-        
-        if let driverData = fetchedReportData.driver {
-            driver = Driver(
-                name: driverData.name!,
-                address: driverData.address!,
-                phoneNumber: driverData.phoneNumber!,
-                driverLicenseNumber: driverData.driverLicenseNumber!,
-                vehicleRegistrationPlate: driverData.vehicleRegistrationPlate!,
-                insuranceCompany: driverData.insuranceCompany!,
-                insurancePolicyNumber: driverData.insurancePolicyNumber!,
-                isAtFault: driverData.isAtFault
-            )
-        }
-        
-        if let otherDriverData = fetchedReportData.otherDriver {
-            otherDriver = Driver(
-                name: otherDriverData.name!,
-                address: otherDriverData.address!,
-                phoneNumber: otherDriverData.phoneNumber!,
-                driverLicenseNumber: otherDriverData.driverLicenseNumber!,
-                vehicleRegistrationPlate: otherDriverData.vehicleRegistrationPlate!,
-                insuranceCompany: otherDriverData.insuranceCompany!,
-                insurancePolicyNumber: otherDriverData.insurancePolicyNumber!,
-                isAtFault: otherDriverData.isAtFault
-            )
-        }
-        let witnesses = fetchedReportData.accidentLocation?.witnesses?.allObjects as? [WitnessData] ?? []
-        let witnessModels = witnesses.map { Witness(name: $0.name ?? "", address: $0.address ?? "", phoneNumber: $0.phoneNumber ?? "") }
-        
-        let accidentDescriptionData = fetchedReportData.accidentDescription
-        accidentDescription = AccidentDescription(notes1: accidentDescriptionData?.notes1 ?? "", notes2: accidentDescriptionData?.notes2 ?? "", vehicleDamage1: (accidentDescriptionData?.vehicleDamage1!)!, vehicleDamage2: (accidentDescriptionData?.vehicleDamage2!)!)
-        
-        return AccidentReport(
-            id: fetchedReportData.idReport!,
-            accidentLocation: AccidentLocation(
-                date: fetchedReportData.accidentLocation?.date ?? Date(),
-                city: fetchedReportData.accidentLocation?.city! ?? "",
-                street: fetchedReportData.accidentLocation?.street! ?? "",
-                houseNumber: fetchedReportData.accidentLocation?.houseNumber! ?? "",
-                kilometerReading: fetchedReportData.accidentLocation?.kilometerReading,
-                injuries: fetchedReportData.accidentLocation?.injuries ?? false,
-                witnesses: witnessModels,
-                otherDamage: fetchedReportData.accidentLocation?.otherDamage ?? false,
-                policeInvolved: fetchedReportData.accidentLocation?.policeInvolved ?? false
-            ),
-            driver: driver ?? Driver(name: "", address: "", phoneNumber: "", driverLicenseNumber: "", vehicleRegistrationPlate: "", insuranceCompany: "", insurancePolicyNumber: ""),
-            otherDriver: otherDriver ?? Driver(name: "", address: "", phoneNumber: "", driverLicenseNumber: "", vehicleRegistrationPlate: "", insuranceCompany: "", insurancePolicyNumber: ""),
-            accidentDescription: accidentDescription
-        )
     }
     
     func removeReport(_ report: AccidentReport) async throws {
@@ -198,22 +78,32 @@ class CoreDataRepository: AccidentReportRepository {
             guard let reportToUpdate = fetchedReports.first else {
                 throw CoreDataError.failedToFindReportToUpdate
             }
-
-            // Update Core Data objects for AccidentLocation, Driver, OtherDriver, AccidentDescription
-            reportToUpdate.accidentLocation = createAccidentLocationData(from: updatedReport.accidentLocation)
-            reportToUpdate.driver = createDriverData(from: updatedReport.driver)
-            if let otherDriver = updatedReport.otherDriver {
-              reportToUpdate.otherDriver = createDriverData(from: otherDriver)
-            } else {
-              // Handle case where otherDriver is nil (optional)
-              reportToUpdate.otherDriver = nil // Or create a default driver object
+            
+            do {
+                let reportData = try JSONEncoder().encode(updatedReport)
+                reportToUpdate.reportData = reportData
+            } catch {
+                print("Error encoding updated accident report data:", error)
             }
-            reportToUpdate.accidentDescription = createAccidentDescriptionData(from: updatedReport.accidentDescription)
-            reportToUpdate.idReport = updatedReport.id
-
+            
             try context.save()
         } catch {
-           throw CoreDataError.coreDataUpdateError
+            throw CoreDataError.coreDataUpdateError
+        }
+    }
+    
+    private func convertFromCoreData(fetchedReportData: AccidentReportData) -> AccidentReport? {
+        guard let reportData = fetchedReportData.reportData else {
+            print("Error: reportData is nil")
+            return nil
+        }
+        
+        do {
+            let report = try JSONDecoder().decode(AccidentReport.self, from: reportData)
+            return report
+        } catch {
+            print("Error decoding accident report data:", error)
+            return nil
         }
     }
 }
